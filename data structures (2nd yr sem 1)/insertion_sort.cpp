931a6bcf823168fac516364896fce7fa07aab5d6
niranjan mule <niranjanmule2024.comp@mmcoe.edu.in>
2025-08-13 20:43:26 +0530

    Add files via upload

---
+++ data structures (2nd yr sem 1)/insertion_sort.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+int cards[20], original[20];
+int size;
+cout << "Enter number of cards : ";
+cin >> size;
+cout << "Enter " << size << " card values:\n";
+for (int i = 0; i < size; i++) {
+cin >> cards[i];
+original[i] = cards[i];
+}
+for (int i = 1; i < size; i++) {
+int key = cards[i];
+int j = i - 1;
+while (j >= 0 && cards[j] > key) {
+cards[j + 1] = cards[j];
+j--;
+}
+cards[j + 1] = key;
+cout << "After step " << i << ": ";
+for (int k = 0; k < size; k++) {
+cout << cards[k] << " ";
+}
+cout << endl;
+}
+cout << "\nOriginal hand: ";
+for (int i = 0; i < size; i++) {
+cout << original[i] << " ";
+}
+cout << endl;
+cout << "Sorted hand: ";
+for (int i = 0; i < size; i++) {
+cout << cards[i] << " ";
+}
+cout << endl;
+return 0;
+}
